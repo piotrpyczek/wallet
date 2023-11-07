965491aade6a640cc2ceea33123a41f032670a20
Piotr Pyczek <piotr.pyczek@hotmail.com>
2023-11-07 10:45:47 +0100

    Get exchange rate endpoint

---
+++ ExchangeRates.API/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,24 @@
+﻿using ExchangeRates.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ExchangeRates.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRatesController : ControllerBase
+    {
+        private readonly IExchangeRateService exchangeRateService;
+
+        public ExchangeRatesController(IExchangeRateService exchangeRateService)
+        {
+            this.exchangeRateService = exchangeRateService;
+        }
+
+        [HttpGet("{code}")]
+        public async Task<ExchangeRate> GetExchangeRateAsync(string code)
+        {
+            return await exchangeRateService.GetCurrentExchageRateAsync(code);
+        }
+    }
+}
---
+++ ExchangeRates.API/Infrastructure/ApplicationConfiguration.cs
@@ -0,0 +1,27 @@
+﻿
+using ExchangeRates.Implementation.Configuration;
+
+namespace ExchangeRates.API.Infrastructure;
+
+public static class ApplicationConfiguration
+{
+    public static IServiceCollection AddApplicationConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient();
+
+        AddApplicationOptions(services, configuration);
+        AddModules(services);
+
+        return services;
+    }
+
+    private static void AddApplicationOptions(IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ExchangeRatesDatabaseSettings>(configuration.GetSection(ExchangeRatesDatabaseSettings.Section));
+    }
+
+    private static void AddModules(IServiceCollection services)
+    {
+        Implementation.Bootstrap.Register(services);
+    }
+}
